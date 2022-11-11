b8e14ee2da8e7a2aa9cfeeedc8218df7020b5006
Slider666 <112625010+Slider666@users.noreply.github.com>
2022-11-11 15:17:42 +0200

    new5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  console.log(shouldResolve);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  // const { delay, step, amount } = event.target.elements;
+  // console.log(delay, step, amount);
+  let delayValue = Number(event.target.delay.value);
+  const stepValue = Number(event.target.step.value);
+  const amountValue = Number(event.target.amount.value);
+  console.log(delayValue, stepValue, amountValue);
+
+  for (let i = 0; i < amountValue; i += 1) {
+    createPromise(i + 1, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += stepValue;
   }
 }
+
+form.addEventListener('submit', onSubmit);
